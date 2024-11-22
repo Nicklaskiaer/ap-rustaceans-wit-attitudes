5065c414470dd75c1d4cf9ac3ff093772ebbc598
nicklas.kiaer <nicklas.kiaer@studenti.unitn.it>
2024-11-22 12:33:37 +0100

    use wg_crate instead of our own

--- src/network_initializer.rs
+++ src/network_initializer.rs
@@ -1,12 +1,12 @@
-use std::{env, fs, thread};
-use std::thread::JoinHandle;
 use internal::config::Drone as ConfigDrone;
-use crate::config::Config;
+use std::thread::JoinHandle;
+use std::{env, fs, thread};
+use wg_2024::wg_config::Config;
 
 use crate::drone::drone_usage::MyDrone;
 use crate::drone::{Drone, DroneOptions};
 
-fn main(){
+fn main() {
     let c = parse_toml();
     let drone_threads = initialize_drones(c.drone);
 
@@ -16,8 +16,7 @@ fn main(){
     // fn crash_handle()
 }
 
-fn initialize_drones(drones: Vec<ConfigDrone>) -> Vec<JoinHandle<()>>{
-
+fn initialize_drones(drones: Vec<ConfigDrone>) -> Vec<JoinHandle<()>> {
     // Vec<Box<dyn Drone>>) -> Vec<JoinHandle<()>>{
     let a = drones[0].id;
 
@@ -45,16 +44,13 @@ fn initialize_drones(drones: Vec<ConfigDrone>) -> Vec<JoinHandle<()>>{
     handles
 }
 
-
-fn parse_toml()->Config{
+fn parse_toml() -> Config {
     let current_path = env::current_dir().expect("Unable to get current directory");
     // println!("Current path: {:?}", current_path);
 
-    let config_data = fs::read_to_string("examples/config/config.toml").expect("Unable to read config file");
+    let config_data =
+        fs::read_to_string("examples/config/config.toml").expect("Unable to read config file");
     let config: Config = toml::from_str(&config_data).expect("Unable to parse TOML");
     //println!("{:#?}", config);
     config
 }
-
-
-
