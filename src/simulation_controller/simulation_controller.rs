c5de948f6348f3bde227eee4617c96cfdb323e0b
nico-cella <nicolo.cellana@studenti.unitn.it>
2025-01-28 22:17:25 +0100

    Updated GUI
    -Small changes to make it more nice visually.
    
    -Now when exit confirmation dialog window shows up the main window below isn't responsive.
    
    -Added SidePanel in log screen for Sorting options of the logs.
    
    -Created topology diagram and linked it to the drones, still need to create an implementation that automatically "scale" and "reorder" the graph automatically.
    
    -Now button to change Packet Drop Rate works.
    
    -Logs have different colors depending on type.

--- src/simulation_controller/simulation_controller.rs
+++ src/simulation_controller/simulation_controller.rs
@@ -57,9 +57,11 @@ impl SimulationController {
 
     pub fn get_drone_ids(&self) -> Vec<String> {
         self.drones.keys()
-            .map(|node_id| node_id.to_string())
+            .map(|node_id| format!("Drone {}", node_id.to_string()))
             .collect()
     }
+
+    //todo(GET_DRONE_CONNECTIONS)
 
     pub fn get_node_event_recv(&self) -> Receiver<DroneEvent>{
         self.node_event_recv.clone()
